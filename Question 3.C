7a7ff9e2c0bb8b3a0d0a5524c25a6bf706d9ca8a
asamnewberta <167760770+asamnewberta@users.noreply.github.com>
2024-04-23 01:30:26 -0700

    Update Question 3.C

--- Question 3.C
+++ Question 3.C
@@ -1,18 +1,16 @@
-def roman_to_decimal(roman):
- roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
- decimal = 0
- prev_value = 0
- for numeral in reversed(roman):
- value = roman_numerals[numeral]
- if value < prev_value:
- decimal -= value
- else:
- decimal += value
- prev_value = value
- return decimal
-def main():
- roman_numeral = input("Enter a Roman numeral: ").upper()
- decimal = roman_to_decimal(roman_numeral)
- print(f"The decimal equivalent of {roman_numeral} is {decimal}")
+def roman_to_decimal(roman_numeral):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    decimal_num = 0
+    
+    for i in range(len(roman_numeral)):
+        if i > 0 and roman_dict[roman_numeral[i]] > roman_dict[roman_numeral[i - 1]]:
+            decimal_num += roman_dict[roman_numeral[i]] - 2 * roman_dict[roman_numeral[i - 1]]
+        else:
+            decimal_num += roman_dict[roman_numeral[i]]
+    
+    return decimal_num
+
 if __name__ == "__main__":
- main()
+    roman_numeral = input("Enter a Roman numeral: ").upper()
+    decimal_num = roman_to_decimal(roman_numeral)
+    print("Decimal representation:", decimal_num)
